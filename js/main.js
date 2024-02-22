478487048afd35a680793dfe0b634bf2daa4fe06
Chris <Christophermn619@gmail.com>
2024-02-22 17:47:53 -0500

    Added js code to create and render the game board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let board;
+
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
+
+/*----- cached element references -----*/
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+function init() {
+   
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+
+    render();
+    
+};
+    
+    //be sure to call the init function!
+init();
+
+function render() {
+
+    board.forEach(function(mark, index) {
+        
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
+        
+        });
+
+    }
