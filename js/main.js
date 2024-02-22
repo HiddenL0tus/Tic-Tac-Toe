ded9b8738b9262d277824a284051f9cf72f68c9d
Chris <Christophermn619@gmail.com>
2024-02-22 17:56:29 -0500

    Added the ability to take turns and switch from X and O

--- js/main.js
+++ js/main.js
@@ -2,22 +2,15 @@
 
 /*----- app's state (variables) -----*/
 let board;
-
-function init() {
-    board = [
-    '', '', '',
-    '', '', '',
-    '', '', ''
-    ];
-    };
-    //be sure to call the init function!
-    init();
+let turn = 'X';
 
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 /*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleTurn);
 
 /*----- functions -----*/
 
@@ -42,7 +35,22 @@ function render() {
         
         //this sets the text content of the square of the same position to the mark on the board. 
         squares[index].textContent = mark;
-        
+
         });
 
     }
+
+    
+function handleTurn(event) {
+    
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+
+    board[idx] = turn;
+    
+    turn = turn === 'X' ? 'O' : 'X';
+
+    render();
+    
+};
