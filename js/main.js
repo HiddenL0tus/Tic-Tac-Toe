9d4ea3abb8ff1f90f809d74d33d974361af14cb5
Chris <Christophermn619@gmail.com>
2024-02-22 18:13:12 -0500

    Added Win logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 /*----- constants -----*/
 
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
+
+win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+
+//win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
 
 /*----- cached element references -----*/
 
 const squares = Array.from(document.querySelectorAll('#board div'));
+
+const messages = document.querySelector('h2');
 
 /*----- event listeners -----*/
 
@@ -38,6 +56,8 @@ function render() {
 
         });
 
+        messages.textContent = `It's ${turn}'s turn!`;
+
     }
 
     
@@ -51,6 +71,23 @@ function handleTurn(event) {
     
     turn = turn === 'X' ? 'O' : 'X';
 
+    win = getWinner();
+
     render();
     
 };
+
+function getWinner() {
+    
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+   
+    });
+    
+    return winner;
+    
+}
+
+messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
